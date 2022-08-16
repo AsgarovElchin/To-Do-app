27d99a5ee44c1b6bdbfd5fe7e4c1e42b2fe12b9a
ElchinCyrus <71493515+ElchinCyrus@users.noreply.github.com>
2022-08-16 21:47:26 +0400

    list_layout_menu done

--- app/src/main/java/com/elchinasgarov/fragments/list/ListFragment.kt
+++ app/src/main/java/com/elchinasgarov/fragments/list/ListFragment.kt
@@ -1,9 +1,9 @@
 package com.elchinasgarov.fragments.list
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
+import androidx.core.view.MenuHost
+import androidx.core.view.MenuProvider
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.elchinasgarov.todoapp.R
@@ -11,14 +11,15 @@ import com.elchinasgarov.todoapp.databinding.FragmentListBinding
 
 
 class ListFragment : Fragment(R.layout.fragment_list) {
-    private lateinit var binding : FragmentListBinding
+    private lateinit var binding: FragmentListBinding
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = FragmentListBinding.inflate(inflater,container,false)
+        binding = FragmentListBinding.inflate(inflater, container, false)
         return binding.root
+
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -30,6 +31,21 @@ class ListFragment : Fragment(R.layout.fragment_list) {
         binding.listLayout.setOnClickListener {
             findNavController().navigate(R.id.action_listFragment_to_updateFragment)
         }
+
+        val menuHost: MenuHost = requireActivity()
+
+        menuHost.addMenuProvider(object : MenuProvider {
+            override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+                // Add menu items here
+                menuInflater.inflate(R.menu.list_fragment_menu, menu)
+            }
+
+            override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+                return true
+            }
+
+
+        })
     }
 
 
