282090bcf49ed923ada513c3616add2f23473726
ElchinCyrus <71493515+ElchinCyrus@users.noreply.github.com>
2022-08-18 19:37:28 +0400

    SharedViewModel done

--- app/src/main/java/com/elchinasgarov/fragments/add/AddFragment.kt
+++ app/src/main/java/com/elchinasgarov/fragments/add/AddFragment.kt
@@ -1,22 +1,32 @@
 package com.elchinasgarov.fragments.add
 
 import android.os.Bundle
+import android.text.TextUtils
 import android.view.*
-import androidx.fragment.app.Fragment
+import android.widget.Toast
 import androidx.core.view.MenuHost
 import androidx.core.view.MenuProvider
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
+import androidx.navigation.fragment.findNavController
+import com.elchinasgarov.data.models.Priority
+import com.elchinasgarov.data.models.ToDoData
+import com.elchinasgarov.data.viewmodel.SharedViewModel
+import com.elchinasgarov.data.viewmodel.ToDoViewModel
 import com.elchinasgarov.todoapp.R
 import com.elchinasgarov.todoapp.databinding.FragmentAddBinding
 
 
 class AddFragment : Fragment() {
-private lateinit var binding : FragmentAddBinding
+    private val mToDoViewModel: ToDoViewModel by viewModels()
+    private val mSharedViewModel: SharedViewModel by viewModels()
+    private lateinit var binding: FragmentAddBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = FragmentAddBinding.inflate(inflater,container,false)
+        binding = FragmentAddBinding.inflate(inflater, container, false)
         return binding.root
     }
 
@@ -31,14 +41,39 @@ private lateinit var binding : FragmentAddBinding
             }
 
             override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+                if (menuItem.itemId == R.id.menu_add) {
+                    insertDataToDb()
+                }
                 return true
 
             }
 
 
-        },viewLifecycleOwner, Lifecycle.State.RESUMED)
+        }, viewLifecycleOwner, Lifecycle.State.RESUMED)
     }
 
+    private fun insertDataToDb() {
+        val mTitle = binding.titleEditText.text.toString()
+        val mPriority = binding.prioritiesSpinner.selectedItem.toString()
+        val mDescription = binding.descriptionEditText.text.toString()
+
+        val validation = mSharedViewModel.verifyDataFromUser(mTitle, mDescription)
+        if (validation) {
+            val newData = ToDoData(
+                0,
+                mTitle,
+                mSharedViewModel.parsePriority(mPriority),
+                mDescription
+            )
+            mToDoViewModel.insertData(newData)
+            Toast.makeText(requireContext(), "Succesfully added!", Toast.LENGTH_SHORT).show()
+            findNavController().navigate(R.id.action_addFragment_to_listFragment)
+        } else {
+            Toast.makeText(requireContext(), "Please fill out all fields", Toast.LENGTH_SHORT)
+                .show()
+        }
+
+    }
 
 
 
